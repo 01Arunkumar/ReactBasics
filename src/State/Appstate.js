d020ec95ab892cbddfc3410aea8b4d67d720652c
01Arunkumar <arunkumarbalure@gmail.com>
2021-06-30 10:24:51 +0530

    state examples

--- src/State/Appstate.js
+++ src/State/Appstate.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Counter from "./components/Counter";
-import Message from "./components/Message";
+import Message from "./Message";
 class App extends React.Component{
   render(){
     return(
